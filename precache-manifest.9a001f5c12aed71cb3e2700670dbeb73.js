9420fdee0fa53c1fcdd0de664a07c703e8fc1862
myatniy <ipsixin3d@gmail.com>
2020-07-25 23:34:26 +0300

    Updates

---
+++ precache-manifest.9a001f5c12aed71cb3e2700670dbeb73.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f202b41ca609fc9fbf941cbb9b27641c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30c4efe8c81af0b5a71c",
+    "url": "/static/css/main.b6276afe.chunk.css"
+  },
+  {
+    "revision": "f616258db325e045f892",
+    "url": "/static/js/2.6ea51e21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6ea51e21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c4efe8c81af0b5a71c",
+    "url": "/static/js/main.a95bbb5a.chunk.js"
+  },
+  {
+    "revision": "3fbf6618dd8a13c25980",
+    "url": "/static/js/runtime-main.2ff5e01d.js"
+  },
+  {
+    "revision": "0738679df4cf4e566f60343830da7bfa",
+    "url": "/static/media/Quicksand-Bold.0738679d.ttf"
+  },
+  {
+    "revision": "c82b184bf7450e14adccb7b0d6117474",
+    "url": "/static/media/Quicksand-SemiBold.c82b184b.ttf"
+  }
+]);
